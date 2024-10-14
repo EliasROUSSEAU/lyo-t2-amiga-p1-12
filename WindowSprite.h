75ee0c8507a800d720734bcfb010b8a5f2b94221
GAMINGCAMPUS\elrousseau <elrousseau@gaming.tech>
2024-10-14 10:31:52 +0200

    issou2

---
+++ WindowSprite.h
@@ -0,0 +1,15 @@
+#ifndef WINDOWSPRITE_H__
+#define WINDOWSPRITE_H__
+
+#include <iostream>
+
+class WindowSprite {
+
+public:
+
+	virtual void Window() = 0;
+	virtual void Sprite() = 0;
+
+};
+
+#endif
