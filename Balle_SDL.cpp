89992868bf27c7ebd2e3bf62ba5d3ed03b2fbaa5
GAMINGCAMPUS\elrousseau <elrousseau@gaming.tech>
2024-10-15 12:11:05 +0200

    Sprite.h + Balle_SDL.h

--- Balle_SDL.cpp
+++ Balle_SDL.cpp
@@ -1,11 +1,44 @@
-//#include "Balle_SDL.h"
-//
-//Balle_SDL::Balle_SDL(int _x, int _y, int _radius)
-//{
-//}
-//
-//Balle_SDL::~Balle_SDL()
-//{
-//}
-//
-//
+#include "Balle_SDL.h"
+
+
+Balle_SDL::Balle_SDL(int _x, int _y, int _radius, SDL_Renderer* _renderer)
+{
+}
+
+Balle_SDL::~Balle_SDL()
+{
+}
+
+void Balle_SDL::LoadTexture()
+{
+}
+
+void Balle_SDL::SetPosition(int _x, int _y)
+{
+}
+
+void Balle_SDL::SetSpeed(int _xspeed, int _yspeed)
+{
+}
+
+void Balle_SDL::GetPosition(int& out_x, int& out_y)
+{
+}
+
+void Balle_SDL::GetSpeed(int& out_xspeed, int& out_yspeed)
+{
+}
+
+void Balle_SDL::GetRadius()
+{
+}
+
+void Balle_SDL::Draw()
+{
+}
+
+void Balle_SDL::Update()
+{
+}
+
+
--- Sprite.h
+++ Sprite.h
@@ -3,8 +3,14 @@
 
 class Sprite {
 public:
-    virtual void draw() = 0;
-    virtual void update() = 0;
+    virtual void LoadTexture() = 0;
+    virtual void SetPosition(int x, int y) = 0;
+    virtual void SetSpeed(int xspeed, int yspeed) = 0;
+    virtual void GetPosition(int& x, int& y) = 0;
+    virtual void GetSpeed(int& xspeed, int& yspeed) = 0;
+    virtual void GetRadius() = 0;
+    virtual void Draw() = 0;
+    virtual void Update() = 0;
 
     virtual ~Sprite() {}
 };
